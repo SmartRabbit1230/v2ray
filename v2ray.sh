079877a42a6afcdcbceef066bf180f6375702f4e
85121115@qq.com <Xyh0920>
2022-01-21 00:15:45 +0800

    测试一下

--- v2ray.sh
+++ v2ray.sh
@@ -2382,8 +2382,10 @@ update_v2ray() {
 }
 update_v2ray.sh() {
 	if [[ $_test ]]; then
+	  echo test
 		local latest_version=$(curl -H 'Cache-Control: no-cache' -s -L "https://raw.githubusercontent.com/SmartRabbit1230/v2ray/test/v2ray.sh" | grep '_version' -m1 | cut -d\" -f2)
 	else
+	  echo master
 		local latest_version=$(curl -H 'Cache-Control: no-cache' -s -L "https://raw.githubusercontent.com/SmartRabbit1230/v2ray/master/v2ray.sh" | grep '_version' -m1 | cut -d\" -f2)
 	fi
 
